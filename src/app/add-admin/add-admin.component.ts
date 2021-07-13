3223de85978d871553bf27b5f44aa11ba399b167
sarthakg23 <sarthakg23@gmail.com>
2021-07-13 17:15:06 +0530

    New Commit

---
+++ src/app/add-admin/add-admin.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LibraryService } from '../library.service';
+
+@Component({
+  selector: 'app-add-admin',
+  templateUrl: './add-admin.component.html',
+  styleUrls: ['./add-admin.component.css']
+})
+export class AddAdminComponent implements OnInit {
+
+  constructor(private libraryService:LibraryService,private router:Router) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit(form:any)
+  {
+    const obj={"user_name":form.username,"user_email":form.useremail,"user_password":form.password,"user_gender":form.usergender,"user_type":"admin","user_age":form.userage,"user_DOB":form.userDOB,"user_address":form.useradress,"user_contact":form.usercontact};
+    this.libraryService.signUp(obj).subscribe(data=>
+      {
+        const mail={"to":form.useremail,"subject":"SignUp SuccessFull","message":"Welcome"+form.username+"\nYou are successfully registered on Library Management System.\n Now you can issue books and download e-books."}
+        this.libraryService.mail(mail).subscribe(data=>{alert("Signup Mail Sent")});
+        this.router.navigate(['/signIn']);
+      })
+  }
+}
--- src/app/view-books-admin/view-books-admin.component.ts
+++ src/app/view-books-admin/view-books-admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LibraryService } from '../library.service';
 
 @Component({
   selector: 'app-view-books-admin',
@@ -7,9 +9,80 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewBooksAdminComponent implements OnInit {
 
-  constructor() { }
+  Title='';
+  Language='';
+  Genre='';
+  showTitle=false;
+  showLang=false;
+  showGenre=false;
+  lists:any=[];
+  rack_no:number|undefined;
+  totalCopies:any;
+  listSelected:number|undefined;
+
+  constructor(private libService:LibraryService,private route:Router) { }
 
   ngOnInit(): void {
+    this.libService.getAllBooks().subscribe((data:any)=>{
+      console.log(data);     
+      this.lists=data;
+    })
+  }
+  addCopy(id:any){
+    const obj={"ID":id}
+    this.libService.addCopy(obj).subscribe((data:any)=>{
+      alert(data);   
+      window.location.reload();
+    })
+  
   }
 
+  deleteCopy(id:any){
+    const obj={"ID":id}
+    this.libService.deleteCopy(obj).subscribe((data:any)=>{
+      alert(data);
+      window.location.reload();
+    })
+   
+  }
+  deleteBook(id:any){
+    this.libService.deleteBook(id).subscribe((data:any)=>{
+      alert(data);
+      window.location.reload();
+    })
+   
+  }
+  
+  updateBook(id:any,rack_no:any){
+    const obj={"ID":id,"RACK_NO":rack_no}
+    console.log(obj);
+    this.libService.updateBook(obj).subscribe((data:any)=>{
+      alert(data);
+      window.location.reload();
+    })
+  
+  }
+
+  clickTitle(){
+  this.showTitle=true;
+  this.showLang=false;
+  this.showGenre=false;
+  
+  }
+  clickLang(){
+    this.showTitle=false;
+  this.showLang=true;
+  this.showGenre=false;
+  }
+  clickGenre(){
+    this.showTitle=false;
+  this.showLang=false;
+  this.showGenre=true;
+  console.log(this.lists);
+  }
+  clickAll(){
+    this.showTitle=true;
+  this.showLang=true;
+  this.showGenre=true;
+  }
 }
