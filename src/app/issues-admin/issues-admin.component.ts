82449d702d1fb011c0689ca750c6de4395032658
rohitOnce <rohitonce127@gmail.com>
2021-07-15 00:47:34 +0530

    issue admin commit

--- src/app/issues-admin/issues-admin.component.ts
+++ src/app/issues-admin/issues-admin.component.ts
@@ -10,11 +10,19 @@ export class IssuesAdminComponent implements OnInit {
 
   constructor(private http:LibraryService) { }
   list:any=[];
+  finelist:any=[];
+  totalfine=0;
 
   ngOnInit(): void {
     this.http.allIssues().subscribe(d=>{
       this.list=d;
     })
+    this.http.calcFine().subscribe(d=>{
+      this.finelist=d;
+    })
+    this.finelist.forEach((element:any) => {
+      this.totalfine=this.totalfine+element.FINE;
+    });
   }
 
 }
